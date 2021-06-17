c8f65b5001067b1857e7ce20ad66fac05d52948a
olga <oluciams84@gmail.com>
2021-06-17 11:01:52 -0500

    Pinta las peliculas utilizando aync / await

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 "use strict";
 
-import{getData} from "./modules/apiRequest.js"
+import{getMovies} from "./modules/apiRequest.js"
 
-window.addEventListener("load", ()=> {
-  getData("popular")
-  .then((data) => data.results.map((movie) => movieCard(movie)))  
-  // .then((data)=>data.results.forEach(movie => {
-  //   movieCard(movie)    
-  // }))
-  .catch(error=>console.warn(error))
+
+
+// const dataAsync = async () => {
+//   const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=$b61364da3cf775b0c719a30ee4fe39b2&language=en-US`)
+//   const data = await response.json()
+//   return data
+// }
+
+//load data using promises
+/*
+window.addEventListener("load",async ()=> { 
+    getMovies("popular")
+    .then((data) => data.results.map((movie) => movieCard(movie)))  
+    .then((data)=>data.results.forEach(movie => {
+      movieCard(movie)    
+    }))
+
+    .catch(error=>console.warn(error))
 })
+*/
 
+window.addEventListener("load",async ()=> { 
+  try{
+    const data = await getMovies("top_rated") 
+      if (data.status_code && !data.success) {
+      throw new Error(`OH NO ERROR try catch ${data.status_message}`)
+      } 
+    data.results.map((movie) => movieCard(movie))
+  }
+  catch (error){
+    console.warn(error)
+  }  
+})
 
 const root = document.getElementById('moviesSection');
 
+/**
+ * 
+ * @param {object} movie movie object 
+ */
 
 const movieCard = ({ title, overview, poster_path, release_date }) => { 
 const row = document.createElement('div');
 row.innerHTML = `
-  <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-        <div class="col-md-4"> 
-          <img class= "moviePoster" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="movie_poster">
-        </div> 
-        <div class="col-md-8">
-          <div class="card-body">
-          <h5 class="card-title">${title}</h5> 
-          <p class="card-text">${overview}</p>
-          <p class="card-text"><small class="text-muted">${release_date}</small></p> 
-        </div> 
-      </div> 
-    </div>
-  </div>
-`;
+  <div class="card mt-4" style="width: 18rem;">       
+      <img class= "card-img-top moviePoster" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="movie_poster">  
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5> 
+      <p class="card-text">${overview}</p>
+      <p class="card-text"><small class="text-muted">${release_date}</small></p> 
+    </div> 
+  </div>   
+    `;
 root.appendChild(row);
 }; 
 
@@ -41,3 +63,9 @@ root.appendChild(row);
 
 
 
+//TODO: 
+/**
+ crear un select
+ el select tendra la opcion de popular o top_rated
+ al seleccionar una u otra debe pintar las peliculas de la categoria que pertenezcan
+ */
--- modules/apiRequest.js
+++ modules/apiRequest.js
@@ -1,22 +1,43 @@
 const apiKey = "b61364da3cf775b0c719a30ee4fe39b2"
 const URL = "https://api.themoviedb.org/3/movie"
-
 const URL_COMPLEMENT = `?api_key=${apiKey}&language=en-US`
 
-export const getData = (ruta) => {
+// Inicio código utilizando Promesas con módulos
+/**
+ * 
+ * @param {string} ruta 
+ * @returns 
+ */
 
-    return fetch(`${URL}/${ruta}${URL_COMPLEMENT}`)
-            .then(response=>response.json())
-            .then(data=>{
+// export const getMovies = (ruta) => {
+//     return fetch(`${URL}/${ruta}${URL_COMPLEMENT}`)
+//             .then(response=>response.json())
+//             .then(data=>{
         
-        if(data.status_code && !data.success){
-            throw new Error(`OH NO ${data.status_message}`)
-        }
-        return data
-    })
-    .catch(err=>console.warn("err", err))
+//         if(data.status_code && !data.success){
+//             throw new Error(`OH NO ${data.status_message}`)
+//         }
+//         return data
+//     })
+//     .catch(err=>console.warn("err", err))
+// }
 
-}
+// Fin código utilizando Promesas con módulos
+
+//  Inicio código utilizando ES6 async / await
+/**
+ * 
+ * @param {string} ruta 
+ * @returns data
+ */
+
+  export const getMovies = async (ruta) => {
+      const response = await fetch(`${URL}/${ruta}${URL_COMPLEMENT}`)
+      const data = await response.json()
+      return data
+  }
+
+//  Fin código utilizando ES6 async / await
 
 
 
